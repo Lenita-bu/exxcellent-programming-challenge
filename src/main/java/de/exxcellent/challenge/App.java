50c1d2184a5343f3cc95ed2ffd8ef659ecda5816
Elena Bunea <translatordeniskina@gmail.com>
2025-03-05 09:19:34 +0100

    Run the weather programme from App.java

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,9 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.analyzer.WeatherAnalyzer;
+
+import java.io.FileNotFoundException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -14,12 +18,26 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        String programmeMode = args[0];
+        String fileName = args[1];
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        switch (programmeMode) {
+            case "--weather":
+                try {
+                    WeatherAnalyzer analyzer = new WeatherAnalyzer(fileName);
+                    int dayWithSmallestTempSpread = analyzer.analyzeSmallestTempSpread();
+                    System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+                } catch (FileNotFoundException e) {
+                    System.out.println(fileName+" not found");
+                }
+                break;
+            case "--football":
+                String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+                System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+                break;
+            default:
+                System.out.println("Unknown programme mode");
+        }
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,7 +3,11 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Example JUnit 5 test case.
@@ -23,6 +27,16 @@ class AppTest {
         assertEquals("successful", successLabel, "My expectations were not met");
     }
 
+    @Test
+    void runWeather() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        App.main("--weather", "weather.csv");
+
+        System.setOut(System.out);
+        assertTrue(outputStream.toString().contains("Day with smallest temperature spread"));
+    }
     @Test
     void runFootball() {
         App.main("--football", "football.csv");
